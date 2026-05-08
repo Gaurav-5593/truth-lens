9d0eb0c6f6636fe0284646e7107b06b314b9fb06
Gaurav Patil <gp3084394@gmail.com>
2026-05-08 08:33:23 +0530

    Fix static files path

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const GROQ_API_KEY = process.env.GROQ_API_KEY || '';
 
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
